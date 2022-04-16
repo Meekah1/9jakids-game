a3dcce00c15936d138e385afb3d115d72f1e37dd
iamakeem <bolarinwa_akeem@yahoo.com>
2022-04-16 10:23:45 +0100

    recent change

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MainHead = styled.div`
+  margin-top: 15px;
+  max-width: 1600px;
+  height: 40px;
+  background-color: #fdc800;
+`;
+
+const Header = () => {
+  return <MainHead>Header</MainHead>;
+};
+
+export default Header;
